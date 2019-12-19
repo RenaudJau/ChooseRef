45f4d48ae4305a82993ad296a028b12a9ea29066
RenaudJau <renaud.jaunatre@irstea.fr>
2019-12-19 17:43:26 +0100

    Ajout fonction DissRef3

--- R/Code_package_ChooseRef.R
+++ R/Code_package_ChooseRef.R
@@ -1,22 +1,96 @@
-#
+########### Fonctions du package ChooseRef ######################
 
-#' Title
-#' @description Calcul de l'erreur standard
+
+
+
+#' Similarities between references and restoration
 #' 
-#' @param RELEVES  -
-#' @param REF -
-#' @param DISTANCES r
-#' @param METHOD r
-#' @param COUL_RELEVES r
-#' @param COUL_Rel_variable r
-#' @param COUL_REF r
-#' @param COUL_Seg r
-#' @param COUL_Seg_variable r
-#' @param CHOIX_NOMBRE r
-#' @param N_REF r
-#' @param DIST_MIN r
-#' @param VAL_DIST r
-#' @param DECAL r
+#' @description Calculate similarity between plots and a group of reference plot
+#'
+#' @param RELEVES Variables of the restoration sites data matrix
+#' @param REF Variables surveys of reference sites data matrix
+#' @param METHOD Dissimilarity index, partial match to "manhattan", "euclidean", "canberra", "clark", "bray", "kulczynski", "jaccard", "gower", "altGower", "morisita", "horn", "mountford", "raup", "binomial", "chao", "cao" or "mahalanobis". Cf \code{\link{vegan::vegdist}} function 
+#' @param BINARY (FALSE by default) If TRUE, data is converted to binary data
+#' @param DOUBLONS (TRUE by default) Are REF plots also included in RELEVES data?
+#' 
+#' @return \item{Diss_Mean}{average dissimilarity by RELEVES}
+#' @return \item{Diss_Min}{minimal dissimilarity by RELEVES}
+#' @return \item{RelRef_order}{list of REF names in ascending order of dissimilarity for each RELEVES}
+#' @return \item{DistRef_order}{list of REF distance values in ascending order of dissimilarity for each RELEVES}
+DissRef3 <- function (RELEVES, REF, METHOD = "bray", BINARY = FALSE, DOUBLONS = TRUE)
+{
+  # Fusion of tables:
+  if(all.equal(names(RELEVES),names(REF))!=TRUE){warning("The two tables do not have the same species")}
+  Tableaux <- rbind(RELEVES,REF)
+  
+  # Dissimilarity matrix:
+  matrice <- as.matrix(vegdist(Tableaux, method = METHOD, binary = BINARY))
+  
+  # Creation of variables:
+  Diss_Mean <- NULL # average dissimilarity of RELEVES
+  Diss_Min <- NULL # minimal dissimilarity of RELEVES
+  
+  # Rel_order will be, for each RELEVES, the list of the names of REF in ascending order of dissimilarity
+  Rel_order <- data.frame(matrix(data = 0, nrow = nrow(RELEVES), ncol = nrow(REF)))
+  row.names(Rel_order) <- row.names(RELEVES) # line names = RELEVES names
+  
+  # ‘Dist_order’: for each RELEVES, the list of distance values to REF in ascending order of dissimilarity
+  # The object is identical to ‘Rel_order’
+  Dist_order <- Rel_order
+  
+  # loop to calculate for each relevé
+  for(i in 1:nrow(RELEVES))
+  {
+    # Ref distances (from the distance matrix)
+    diss_qn <- matrice[i,c((nrow(RELEVES)+1):(nrow(RELEVES)+nrow(REF)))]
+    
+    # ‘if’ & ‘else’: to delete a zero in the distance matrix when there is a duplicate of data in RELEVES and in REF (in this case set DOUBLONS = TRUE) and we have a value 0 between the RELEVES i and at  least one of REF
+    
+    if(DOUBLONS==TRUE & min(diss_qn)==0)
+    {
+      diss_qnw0_2 <- diss_qn[-c(1:length(diss_qn))[diss_qn==0][1]]
+    } else {
+      diss_qnw0_2 <- diss_qn
+    }
+    
+    # Placing the REF names in ascending order according to the distance to the RELEVES i
+    Rel_order[i,] <- factor(row.names(REF)[order(diss_qn)])
+    
+    # Placing the distance values in ascending order according to the distance to RELEVES i
+    Dist_order[i,] <- diss_qn[order(diss_qn)]
+    
+    # Average distance value
+    Diss_Mean[i] <- mean(diss_qnw0_2)
+    
+    # Minimum distance value
+    Diss_Min[i] <- min(diss_qnw0_2)
+    
+  }
+  
+  Output <- list(Diss_Mean,Diss_Min,Rel_order,Dist_order)
+  names(Output) <- c("Diss_Mean","Diss_Min","RelRef_order","DistRef_order")
+  return(Output)
+}
+
+
+
+#' Plot of references and restoration site dissimilarities
+#' @description Display similarity link (previously calculated with \code{\link{ChosseRef::DissRef3}} function) between plots and a group of reference plot
+#' 
+#' @param RELEVES  Variables of the restoration sites data matrix
+#' @param REF Variables of reference sites data matrix
+#' @param DISTANCES Object resulting from the analysis of the \code{\link{ChosseRef::DissRef3}} function.
+#' @param METHOD Dissimilarity index, partial match to "manhattan", "euclidean", "canberra", "clark", "bray", "kulczynski", "jaccard", "gower", "altGower", "morisita", "horn", "mountford", "raup", "binomial", "chao", "cao" or "mahalanobis". Cf \code{\link{vegan::vegdist}} function 
+#' @param COUL_RELEVES Color of RELEVES names
+#' @param COUL_Rel_variable If TRUE: color change for each RELEVES
+#' @param COUL_REF Color of REF names
+#' @param COUL_Seg Color of links
+#' @param COUL_Seg_variable If TRUE: color change for each link
+#' @param CHOIX_NOMBRE Display of the reference number \code{N_REF} or display of the distance value \code{DIST_MIN}.
+#' @param N_REF Number of REF to display a link between REF and RELEVES
+#' @param DIST_MIN Minimum distance value to display a link between REF and RELEVES
+#' @param VAL_DIST Distance value display
+#' @param DECAL Distance between the printing of the distance and the link
 Diss_Ref_Plot <- function(RELEVES, REF, DISTANCES,
                           METHOD = "euclidean",
                           COUL_RELEVES = 2, COUL_Rel_variable = TRUE,
@@ -98,6 +172,4 @@ Diss_Ref_Plot <- function(RELEVES, REF, DISTANCES,
   }
 }
 
-library(devtools)
-install_github("RenaudJau/ChooseRef")
-?Diss_Ref_Plot
+
